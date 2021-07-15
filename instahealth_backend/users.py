ca5092a32ff671f26c249903d192f3cca725f760
George Zhang <geetransit@gmail.com>
2021-07-15 15:06:27 -0400

    Rename username -> email

--- instahealth_backend/users.py
+++ instahealth_backend/users.py
@@ -19,17 +19,17 @@ ROLES = [
 def login():
     try:
         data = request.get_json(force=True)
-        username = data["username"]
+        email = data["email"]
         password = data["password"]
         db = get_db()
 
         row = db.execute(
-            "SELECT * FROM users WHERE username = ?",
-            [username],
+            "SELECT * FROM users WHERE email = ?",
+            [email],
         ).fetchone()
 
         if row is None:
-            raise ValueError("username is not registered")
+            raise ValueError("email is not registered")
         if not check_password_hash(user["password"], password):
             raise ValueError("password is incorrect")
 
@@ -42,29 +42,29 @@ def login():
 def register():
     try:
         data = request.get_json(force=True)
-        username = data["username"]
+        email = data["email"]
         password = data["password"]
         role = data["role"]
         db = get_db()
 
-        if not username:
-            raise ValueError("username is required")
+        if not email:
+            raise ValueError("email is required")
         if not password:
             raise ValueError("password is required")
         if role not in ROLES:
             raise ValueError(f"role not in {', '.join(ROLES)}")
         if db.execute(
-            "SELECT id FROM users WHERE username = ?",
-            [username],
+            "SELECT id FROM users WHERE email = ?",
+            [email],
         ).fetchone() is not None:
-            raise ValueError("username already registered")
+            raise ValueError("email already registered")
 
         db.execute(
             (
-                "INSERT INTO users (username, password, role)"
+                "INSERT INTO users (email, password, role)"
                 " VALUES (?, ?, ?)"
             ),
-            [username, generate_password_hash(password), role],
+            [email, generate_password_hash(password), role],
         )
         db.commit()
 
