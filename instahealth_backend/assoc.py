2d60a1b23ea2253e06a9e16d5f4391fa8e22fcf0
George Zhang <geetransit@gmail.com>
2021-07-15 16:11:56 -0400

    Fix more bugs

--- instahealth_backend/assoc.py
+++ instahealth_backend/assoc.py
@@ -2,12 +2,12 @@ from flask import current_app
 
 _assoc_dict = None
 
-def get_assoc_dict(text):
+def get_assoc_dict():
     global _assoc_dict
     if _assoc_dict is None:
         assoc_dict = {}
         with current_app.open_resource("assoc.txt") as file:
-            for line in file:
+            for line in file.read().decode("utf-8").splitlines():
                 tag, _, rest = line.partition(" ")
                 for word in line.split(", "):
                     if word not in assoc_dict:
--- instahealth_backend/questions.py
+++ instahealth_backend/questions.py
@@ -1,3 +1,5 @@
+import re
+
 from flask import Blueprint, request, jsonify
 from werkzeug.security import check_password_hash, generate_password_hash
 
