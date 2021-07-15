926351c4b067ce18a565e9f8a59a7873c873e184
George Zhang <geetransit@gmail.com>
2021-07-15 14:12:42 -0400

    Added login and register endpoints

--- instahealth_backend/users.py
+++ instahealth_backend/users.py
@@ -1,15 +1,74 @@
-from flask import Blueprint
+from flask import Blueprint, Response, request
+from werkzeug.security import check_password_hash, generate_password_hash
 
 from .db import get_db
 
 bp = Blueprint('users', __name__)
 
-@bp.route("/login")
-def login():
-    db = get_db()
-    return "login get"
+ROLES = [
+    "patient",
+    "family-doctor",
+    "cardiologist",
+    "dermatologist",
+    "optometrist",
+    "dentist",
+    "otolaryngologist",
+]
 
-@bp.route("/register")
+@bp.route("/login", methods=["POST"])
+def login():
+    try:
+        data = request.get_json(force=True)
+        email = data["email"]
+        password = data["password"]
+        db = get_db()
+
+        row = db.execute(
+            "SELECT * FROM users WHERE email = ?",
+            [email],
+        ).fetchone()
+
+        if row is None:
+            raise ValueError("email is not registered")
+        if not check_password_hash(user["password"], password):
+            raise ValueError("password is incorrect")
+
+    except Exception as e:
+        return str(e), 400
+    else:
+        return "", 200
+
+@bp.route("/register", methods=["POST"])
 def register():
-    db = get_db()
-    return "register get"
+    try:
+        data = request.get_json(force=True)
+        email = data["email"]
+        password = data["password"]
+        role = data["role"]
+        db = get_db()
+
+        if not email:
+            raise ValueError("email is required")
+        if not password:
+            raise ValueError("password is required")
+        if role not in ROLES:
+            raise ValueError(f"role not in {', '.join(ROLES)}")
+        if db.execute(
+            "SELECT id FROM users WHERE email = ?",
+            [email],
+        ).fetchone() is not None:
+            raise ValueError("email already registered")
+
+        db.execute(
+            (
+                "INSERT INTO users (email, password, role)"
+                " VALUES (?, ?, ?)"
+            ),
+            [email, generate_password_hash(password), role],
+        )
+        db.commit()
+
+    except Exception as e:
+        return repr(e), 400
+    else:
+        return "", 200
