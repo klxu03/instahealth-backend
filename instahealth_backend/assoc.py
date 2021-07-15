9aa5bff7a0b09e6085765ab0320ed007ae703428
George Zhang <geetransit@gmail.com>
2021-07-15 16:04:35 -0400

    Added the word association stuff

---
+++ instahealth_backend/assoc.py
@@ -0,0 +1,17 @@
+from flask import current_app
+
+_assoc_dict = None
+
+def get_assoc_dict(text):
+    global _assoc_dict
+    if _assoc_dict is None:
+        assoc_dict = {}
+        with current_app.open_resource("assoc.txt") as file:
+            for line in file:
+                tag, _, rest = line.partition(" ")
+                for word in line.split(", "):
+                    if word not in assoc_dict:
+                        assoc_dict[word] = set()
+                    assoc_dict[word].add(tag)
+        _assoc_dict = assoc_dict
+    return _assoc_dict
